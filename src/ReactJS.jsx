a48eee4545f5d09b25945a36a2d465b7b0f5b146
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-01 00:20:57 +0100

    add button to react component

--- src/ReactJS.jsx
+++ src/ReactJS.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const ReactJS =() => {
+const ReactJS = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: '/redux',
+    };
+
+    history.push(location);
+  };
   return (
     <article>
       <h2>ReactJS</h2>
+      <button onClick={handleOnClick}>Idź do Redux</button>
     </article>
   );
 };
