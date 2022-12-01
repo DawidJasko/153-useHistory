77f3d7deaa577ce4818a8c6ffa30579f62c73b73
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-01 22:33:36 +0100

    useLocation

--- src/Redux.jsx
+++ src/Redux.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Redux = () => {
   const history = useHistory();
+  const location = useLocation();
 
   const handleOnClick = () => history.goBack();
+
+  const isActive = Boolean(
+    history.location.state && history.location.state.isActive
+  );
 
   return (
     <article>
       <h2>Redux</h2>
+      <p>Przesłana informacja isActive: {String(isActive)}</p>
       <button onClick={handleOnClick}>Wróć do poprzedniej strony</button>
     </article>
   );
