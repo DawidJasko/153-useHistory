43d9279ee1db2b02686f65e49b4d4fc3ec661c90
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-01 22:51:28 +0100

    add useParams

--- src/Mobx.jsx
+++ src/Mobx.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Mobx =() => {
+const Mobx = () => {
+  const [inputData, setInputData] = useState('');
+  const history = useHistory();
+
+  const handleOnChange = (event) => setInputData(event.target.value);
+
+  const handleOnClick = () => {
+    const location = {
+      pathname: `/typescript/${inputData}`,
+    };
+
+    history.push(location);
+  };
   return (
     <article>
       <h2>Mobx</h2>
+      <input onChange={handleOnChange} type="text" value={inputData} />
+      <button onClick={handleOnClick}> Wyślij params to TypeDcript</button>
     </article>
   );
 };
--- src/TypeScript.jsx
+++ src/TypeScript.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const TypeScript = () => {
+  const { message } = useParams();
+
+  console.log(message);
+
   return (
     <article>
       <h2>TypeScript</h2>
+      <p>{message}</p>
     </article>
   );
 };
